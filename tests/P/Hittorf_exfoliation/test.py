26f4c0407c464d4b8748081645c33b8f552cc058
gabor <gabor@gc121mac.eng.cam.ac.uk>
2020-04-29 14:01:17 +0100

    added reference data to Hittorf scans

--- tests/P/Hittorf_exfoliation/test.py
+++ tests/P/Hittorf_exfoliation/test.py
@@ -1,6 +1,6 @@
 from utilities import path_of_file, evaluate_file
 
-filenamelist = ["Hittorf_ActaB1969.vasp_STEPS.xyz", "Hittorf_Angew2020.vasp_STEPS.xyz"]
+filenamelist = ["Hittorf_scan_ActaB1969_mbd.xyz",   "Hittorf_scan_Angew2020_mbd.xyz"]
 
 for filename in filenamelist: 
     evaluate_file(path_of_file(__file__)+"/"+filename)
